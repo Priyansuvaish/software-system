1a7940d8e73a55fa0d18d4273fbf0ae53f61a3f9
Priyanshu <priyanshu@kali.Priyanshu>
2023-10-07 07:46:01 -0400

    updated

---
+++ HandOn2/31a.c
@@ -0,0 +1,24 @@
+/*
+Name : 15.c
+Author : Priyanshu Vaish
+Description : Write a program to create a semaphore and initialize value to the semaphore.
+         a. create a binary semaphore
+*/
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+union semun {
+  int val;      // value for SETVAL 
+  struct semid_ds *buf;  // buffer for IPC_STAT
+  unsigned short int *array;  // array for GETALL
+ };
+
+int main() {
+    key_t key = ftok("m", 0);
+    int sem_id = semget(key, 1, IPC_CREAT|0644);
+    union semun arg;
+    arg.val = 1;
+
+    semctl(sem_id, 0, SETVAL, arg);
+}
---
+++ HandOn2/31b.c
@@ -0,0 +1,24 @@
+/*
+Name : 31b.c
+Author : Priyanshu Vaish
+Description : Write a program to create a semaphore and initialize value to the semaphore.
+         a. create a counting semaphore
+*/
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+union semun {
+  int val;      // value for SETVAL 
+  struct semid_ds *buf;  // buffer for IPC_STAT
+  unsigned short int *array;  // array for GETALL
+ };
+
+int main() {
+    key_t key = ftok("sm", 10);
+    int sem_id = semget(key, 2, IPC_CREAT|0644);
+    union semun arg;
+    arg.val = 2;
+
+    semctl(sem_id, 1, SETVAL, arg);
+}
